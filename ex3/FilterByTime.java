d125ff29c15411210680a0776b83fba0aea6e5c1
Tony <779379672@qq.com>
2016-11-03 15:27:32 +0800

    update3
    pzx

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -1,11 +1,16 @@
 package No3;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -17,22 +22,84 @@ import java.util.Scanner;
  */
 public class FilterByTime {
 	
-	public static void main(String[] args) throws ParseException, FileNotFoundException{
+	public static void main(String[] args) throws ParseException, IOException{
+		
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
-		FileInputStream inputStream = new FileInputStream(filePath);
-		Scanner scanner = new Scanner(inputStream, "UTF-8");
-		while (scanner.hasNext()){
-			// 对每行进行处理
-			String line = scanner.nextLine();
-			// 切分获取IP，Time
-			String strIp = null;
-			String strTime = null;
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+		//正则表达式提取出ip和时间
+		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[(\\d+\\/[JFMAONSD][a-z]{2}\\/\\d{4}:\\d{2}:\\d{2}:\\d{2}) .*";
+		Pattern r = Pattern.compile(pattern);
+        //从本地文件读入sccess.log，使用字符流读取
+		FileReader fileRead = new FileReader("F:/access.log");
+		BufferedReader br = new BufferedReader(fileRead);
+		//读出一行寄存处
+		String str=null;
+		
+		while((str=br.readLine())!=null){
+			Matcher m = r.matcher(str);
+			String ip = null;
+			String tm = null;
+			if(m.find()){
+				ip = m.group(1);
+				tm = m.group(2);
+			}
+			//调用getTimes方法“31/Dec/2015:18:59:26”格式的提起转换为“yyyy-MM-dd HH:mm:ss”格式并且得到时间戳
+			long time=getTimes(tm);
+			//用时间戳比较获取时间区间符合的ip和时间
+			if(time>=beginDate.getTime() && time<=endDate.getTime()){
+				System.out.println(ip+"\t"+tm);
+			}
 		}
+		br.close();
+		fileRead.close();
 	}
 	
+	private static long getTimes(String str) throws ParseException {
+		String[] strs = str.split("/");
+		String num = null;
+		switch (strs[1]) {
+		case "Jan":
+			num = "01";
+			break;
+		case "Feb":
+			num = "02";
+			break;
+		case "Mar":
+			num = "03";
+			break;
+		case "Apr":
+			num = "04";
+			break;
+		case "May":
+			num = "05";
+			break;
+		case "Jun":
+			num = "06";
+			break;
+		case "Jul":
+			num = "07";
+			break;
+		case "Aug":
+			num = "08";
+			break;
+		case "Sep":
+			num = "09";
+			break;
+		case "Oct":
+			num = "10";
+			break;
+		case "Nov":
+			num = "11";
+			break;
+		case "Dec":
+			num = "12";
+			break;
+		}
+		String t2 = str.replaceAll(strs[1], num);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss");
+		Date date = simpleDateFormat.parse(t2);
+		return date.getTime();
+	}
 }
